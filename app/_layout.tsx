62b2c27cf828aadd47031ca031e4b32456817e84
ArKharMinn <arkharminn.1@gmail.com>
2024-11-01 15:39:11 +0630

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import {Provider} from 'react-redux'
+import store from '@/redux/store'
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Provider store={store}>
+      <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="number/[id]" options={{ headerShown: false }} />
     </Stack>
+    </Provider>
   );
 }
